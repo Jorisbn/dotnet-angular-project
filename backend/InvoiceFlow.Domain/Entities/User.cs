2db280fe580c29875c2599546afac80a15dca838
Joris <jorisbruynen@outlook.com>
2026-09-23 15:41:24 +0200

    Feat: Created pages, router, and pages that use a layout.

--- backend/InvoiceFlow.Domain/Entities/User.cs
+++ backend/InvoiceFlow.Domain/Entities/User.cs
@@ -3,9 +3,9 @@ namespace InvoiceFlow.Domain.Entities;
 public class User
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; } = null;
-    public string LastName { get; set; } = null;
-    public string Email { get; set; } = null;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public bool IsActive { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Home } from './pages/home/home';
+import { Invoices } from './pages/invoices/invoices';
+import { Payments } from './pages/payments/payments';
+import { Users } from './pages/users/users';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+      {
+        path: 'invoices',
+        component: Invoices,
+      },
+      {
+        path: 'payments',
+        component: Payments,
+      },
+      {
+        path: 'users',
+        component: Users,
+      },
+    ],
+  },
+];
